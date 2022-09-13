a01f9e7dd0f9abc37c08bdd6e74edccdb300383a
mrhotio <26902309+mrhotio@users.noreply.github.com>
2022-09-13 09:44:54 +0200

    skip ci

---
+++ update-versions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+version=$(curl -fsSL "https://readarr.servarr.com/v1/update/nightly/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -r .[0].version)
+[[ -z ${version} ]] && exit 0
+[[ ${version} == "null" ]] && exit 0
+version_json=$(cat ./VERSION.json)
+jq '.version = "'"${version}"'"' <<< "${version_json}" > VERSION.json
