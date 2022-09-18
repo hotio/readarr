50faf18ac71a44d221781d448265c419024ad3ee
mrhotio <26902309+mrhotio@users.noreply.github.com>
2022-09-18 06:52:34 +0200

    use branch var

--- update-versions.sh
+++ update-versions.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-version=$(curl -fsSL "https://readarr.servarr.com/v1/update/nightly/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -r .[0].version)
+branch="nightly"
+version=$(curl -fsSL "https://readarr.servarr.com/v1/update/${branch}/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -r .[0].version)
 [[ -z ${version} ]] && exit 0
 [[ ${version} == "null" ]] && exit 0
 version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'"' <<< "${version_json}" > VERSION.json
+jq '.version = "'"${version}"'" | .branch = "'"${branch}"'"' <<< "${version_json}" > VERSION.json
