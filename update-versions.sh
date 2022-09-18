9ba6d1fa2c0a39ba4265e3011b96c30c766ef1cc
mrhotio <26902309+mrhotio@users.noreply.github.com>
2022-09-18 07:29:16 +0200

    fix

--- update-versions.sh
+++ update-versions.sh
@@ -5,4 +5,4 @@ version=$(curl -fsSL "https://readarr.servarr.com/v1/update/${branch}/changes?os
 [[ -z ${version} ]] && exit 0
 [[ ${version} == "null" ]] && exit 0
 version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'" | .branch = "'"${branch}"'"' <<< "${version_json}" > VERSION.json
+jq '.version = "'"${version}"'" | .sbranch = "'"${branch}"'"' <<< "${version_json}" > VERSION.json
